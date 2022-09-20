45e72a600a9d54728f2747cb89674d6120a5371c
Velthas <damianobattaglia97@gmail.com>
2022-09-20 22:26:44 +0200

    added tests for calculator

---
+++ calculator/calculator.test.js
@@ -0,0 +1,31 @@
+test('using add method, 2 + 2 returns 4', () => {
+    expect(calculator.add(2, 2)).toBe(4);
+});
+
+test('using add method, 386 + 492 returns 878', () => {
+    expect(calculator.add(386, 492)).toBe(878);
+});
+
+test('using subtract method, 2 - 2 returns 0', () => {
+    expect(calculator.subtract(2, 2)).toBe(0);
+});
+
+test('using subtract method, 386 - 492 returns -106', () => {
+    expect(calculator.subtract(386, 492)).toBe(-106);
+});
+
+test('using multiply method, 2 * 2 returns 4', () => {
+    expect(calculator.multiply(2, 2)).toBe(4);
+});
+
+test('using multiply method, 7 * 0 returns 0', () => {
+    expect(calculator.multiply(7, 0)).toBe(0);
+});
+
+test('using multiply method, 2 : 2 returns 1', () => {
+    expect(calculator.divide(2, 2)).toBe(1);
+});
+
+test('using multiply method, 7 : 0 returns bruh', () => {
+    expect(calculator.divide(7, 0)).toBe(Infinity);
+});
