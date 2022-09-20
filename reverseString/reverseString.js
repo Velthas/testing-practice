6c6a8da688995c32905aa21742e84a5f216bf867
Velthas <damianobattaglia97@gmail.com>
2022-09-20 12:13:15 +0200

    added code for reverseString, tests ok

---
+++ reverseString/reverseString.js
@@ -0,0 +1,10 @@
+export default reverseString
+
+// Takes the initial string, then turns it into an array of separate
+// letters using the split method with '' as a parameter,
+// that array is then reversed, and finally put together into a string using join.
+// Ff no argument is passed, join returns elements as comma separated list
+// but I wanted it to return each letter stitched together, so passed '';
+function reverseString(string) {
+    return string.split('').reverse().join('');
+}
