ac4e41a2d35c2d274f83ce4106b0654d9855215b
Velthas <damianobattaglia97@gmail.com>
2022-09-23 12:45:18 +0200

    fixed test problems

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -5,22 +5,16 @@ test('string "attack at dawn" with shift of 5 returns "fyyfhp fy ifbs"', () => {
 });
 
 test('string "attack at dawn" with shift of 11 returns "leelnv le olhy"', () => {
-  expect(() => {
-    caesarCipher.shift(11);
-    return caesarCipher.cipher('attack at dawn');
-  }).toBe('leelnv le olhy');
+  caesarCipher.shift(11);
+  expect(caesarCipher.cipher('attack at dawn')).toBe('leelnv le olhy');
 });
 
 test('function correctly handles capitalized words.', () => {
-  expect(() => {
-    caesarCipher.shift(5);
-    return caesarCipher.cipher('AtTaCk aT DaWn');
-  }).toBe('FyYfHp fY IfBs');
+  caesarCipher.shift(5);
+  expect(caesarCipher.cipher('AtTaCk aT DaWn')).toBe('FyYfHp fY IfBs');
 });
 
 test('handles a negative shift of -2', () => {
-  expect(() => {
-    caesarCipher.shift(-2);
-    return caesarCipher.cipher('attack at dawn') 
-  }).toBe('yrryai yr byul');
+  caesarCipher.shift(-2);
+  expect(caesarCipher.cipher('attack at dawn')).toBe('yrryai yr byul');
 })
