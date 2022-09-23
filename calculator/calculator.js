8021a503ec749a1c925284c3c0698159418cb895
Velthas <damianobattaglia97@gmail.com>
2022-09-23 15:36:57 +0200

    linted calculator code & tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,11 @@
-export { calculator }
+export { calculator };
 
-const calculator = (function (){
-function add(a, b) { return a + b };
-function subtract(a, b) { return a - b };
-function multiply(a, b) { return a * b };
-function divide(a, b) { return a / b };
-
-return { add, subtract, multiply, divide };
-})();
+const calculator = (function () {
+  function add(a, b) { return a + b; }
+  function subtract(a, b) { return a - b; }
+  function multiply(a, b) { return a * b; }
+  function divide(a, b) { return a / b; }
+  return {
+    add, subtract, multiply, divide,
+  };
+}());
